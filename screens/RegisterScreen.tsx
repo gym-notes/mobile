032aafa2936f15657cef30327161530cf16078c2
Kamil Nahotko <kamilnahotko@gmail.com>
2021-12-28 16:35:55 +0100

    feat: add back button in register screen

--- screens/RegisterScreen.tsx
+++ screens/RegisterScreen.tsx
@@ -1,20 +1,30 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { NavigationProp, ParamListBase } from '@react-navigation/native';
 import RegisterIndicator from '../components/RegisterIndicator';
 import RegisterForm from '../components/RegisterForm';
+import Button from '../components/Button';
 
-interface Props {
+interface IRegisterScreen {
   navigation: NavigationProp<ParamListBase>;
 }
 
-const RegisterScreen: React.FC<Props> = () => {
+const RegisterScreen: React.FC<IRegisterScreen> = ({ navigation }) => {
   const [currentStep, setCurrentStep] = useState(0);
   const steps = ['Login details', 'User Profile', 'User Profile'];
 
   return (
     <View style={styles.container}>
       <View style={styles.wrapper}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Button
+            title="log in"
+            backgroundColor="#2E2C39"
+            textColor="#BCBCC0"
+            width={100}
+            iconName="arrow-left"
+          />
+        </TouchableOpacity>
         <Text style={styles.headerText}>Sign Up</Text>
         <RegisterIndicator currentStep={currentStep} steps={steps} />
         <RegisterForm steps={steps} setCurrentStep={setCurrentStep} currentStep={currentStep} />
